551bf1d192ae7438655a757646ca175b8f7fc541
Stanislav Synko <safitudo@gmail.com>
2023-10-11 19:05:44 -0700

    first js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,89 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperator = '';
+let resetScreen = false;
+
+const display = document.querySelector('#display');
+
+const updateDisplay = () => {
+    display.value = secondOperand;
+};
+
+const appendNumber = (number) => {
+    if (resetScreen) {
+        display.value = '';
+        resetScreen = false;
+    }
+    secondOperand = secondOperand.toString() + number.toString();
+};
+
+const chooseOperation = (operator) => {
+    if (secondOperand === '') return;
+    if (firstOperand !== '') {
+        compute();
+    }
+    currentOperator = operator;
+    firstOperand = secondOperand;
+    secondOperand = '';
+};
+
+const compute = () => {
+    let computation;
+    const prev = parseFloat(firstOperand);
+    const current = parseFloat(secondOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (currentOperator) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    secondOperand = computation;
+    currentOperator = undefined;
+    firstOperand = '';
+    resetScreen = true;
+};
+
+const clear = () => {
+    firstOperand = '';
+    secondOperand = '';
+    currentOperator = undefined;
+};
+
+document.querySelector('#buttons').addEventListener('click', (button) => {
+    if (button.target.matches('button')) {
+        const action = button.target.value;
+        switch (action) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                chooseOperation(action);
+                break;
+            case '=':
+                compute();
+                updateDisplay();
+                break;
+            case 'AC':
+                clear();
+                updateDisplay();
+                break;
+            default:
+                if (Number.isInteger(parseFloat(action))) {
+                    appendNumber(action);
+                    updateDisplay();
+                }
+        }
+    }
+});
 //js logic here
